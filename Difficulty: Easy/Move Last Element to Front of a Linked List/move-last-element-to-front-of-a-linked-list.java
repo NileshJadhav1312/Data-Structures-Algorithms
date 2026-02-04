6a1f02bd2d0e99841075d1c5dc4449e84923f5a1
Nilesh Jadhav <nileshjadhav93078@gmail.com>
2026-02-04 19:19:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move Last Element to Front of a Linked List/move-last-element-to-front-of-a-linked-list.java
@@ -0,0 +1,45 @@
+/*
+
+Definition for singly Link List Node
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+You can also use the following for printing the link list.
+Node.printList(Node node);
+*/
+
+class Solution {
+    public static Node moveToFront(Node head) {
+       Node lastnode=head;
+       
+       while(lastnode.next!=null)
+        {
+          lastnode=lastnode.next;   
+        }
+       
+        Node newnode = new Node(lastnode.data);
+
+         
+         newnode.next=head;
+         head=newnode;
+       
+       
+       Node curr=head;
+        while(curr.next.next!=null)
+        {
+         curr=curr.next;
+          
+       }
+          curr.next=null;
+     
+      return head;   
+    }
+}
