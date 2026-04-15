1a7c99c5a54965b19973d302e366d5897278b1ad
Nilesh Jadhav <nileshjadhav93078@gmail.com>
2026-04-15 21:15:39 +0530

    Time: 1 ms (78.77%), Space: 42.3 MB (82.56%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,22 +1,27 @@
+import java.util.HashSet;
+
 class Solution {
-    public boolean isHappy(int n) 
-    { 
-    int slow=n;
-    int fast=n;
-    do{
-        slow=square(slow);
-        fast=square(square(fast));
+    public boolean isHappy(int n) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (n != 1) {
+            if (seen.contains(n)) {
+                return false; // cycle detected
+            }
+            seen.add(n);
+            n = square(n);
+        }
+
+        return true;
     }
-    while(slow!=fast);
-   return slow==1;
-    }
-     int square(int num) {
+
+    int square(int num) {
         int sum = 0;
 
-        while (num > 0) {
-            int rem = num % 10;
-            sum += rem * rem;
-            num /= 10;
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num = num / 10;
         }
 
         return sum;
