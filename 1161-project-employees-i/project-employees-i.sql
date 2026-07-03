525e3f9f025f41bf8004abe973195cbf8bd79521
Nilesh Jadhav <188693536+NileshJadhav1312@users.noreply.github.com>
2026-07-03 09:04:49 +0530

    Time: 583 ms (23.74%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,7 +1,7 @@
 SELECT p.project_id,                                                 -- Select project ID
        ROUND(avg(experience_years),2)                                -- avg rounded to 2 decimals
        AS average_years                                              -- Rename result column as average_years
-FROM Project p                                                       -- Project table with alias 'p'
-JOIN Employee e                                                      -- Join Employee table with alias 'e'
-ON p.employee_id = e.employee_id                                     -- Match employee IDs from both tables
+FROM Project p                                                   -- Project table with alias 'p'
+JOIN Employee e                                                 -- Join Employee table with alias 'e'
+ON p.employee_id = e.employee_id                               -- Match employee IDs from both tables
 GROUP BY project_id;                                                 -- Group rows by project_id
