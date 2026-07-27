7891260e8af56a2062ac9fb0506d2960c9148112
Nilesh Jadhav <188693536+NileshJadhav1312@users.noreply.github.com>
2026-07-27 18:30:23 +0530

    Time: 1674 ms (28.67%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,18 +1,24 @@
-SELECT
-    'Low Salary' As category
-    ,COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
-FROM accounts
+select 'Low Salary' as category ,count(*) as accounts_count from Accounts where income<20000
+union all
+select 'Average Salary' ,count(*)  from Accounts where income between 20000 and 50000
+union all
+select 'High Salary' ,count(*)from Accounts where income>50000
 
-UNION ALL
+-- SELECT
+--     'Low Salary' As category
+--     ,COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
+-- FROM accounts
 
-SELECT
-    'Average Salary' AS category
-    ,COUNT(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END) AS accounts_count
-FROM accounts
+-- UNION ALL
 
-UNION ALL
+-- SELECT
+--     'Average Salary' AS category
+--     ,COUNT(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END) AS accounts_count
+-- FROM accounts
 
-SELECT 
-    'High Salary' AS category
-    ,COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
-FROM accounts
+-- UNION ALL
+
+-- SELECT 
+--     'High Salary' AS category
+--     ,COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
+-- FROM accounts
