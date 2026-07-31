a46b8e466c4ae4ca0af9de9ead7a9f61dcc04b33
Nilesh Jadhav <188693536+NileshJadhav1312@users.noreply.github.com>
2026-07-31 23:01:39 +0530

    Time: 432 ms (13.85%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,4 @@
 -- # Write your MySQL query statement below
--- select
--- CASE
---     WHEN id % 2 = 0 THEN id+1
---     ELSE id-1
--- END, student from Seat order by id asc;
 SELECT
     CASE
         WHEN id % 2 = 1 AND id+1 IN(SELECT id FROM Seat) THEN id + 1
