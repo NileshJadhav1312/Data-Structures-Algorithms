62f3f32d2d9b2558bbf7ea37ac88062b27b3df30
Nilesh Jadhav <188693536+NileshJadhav1312@users.noreply.github.com>
2026-08-08 22:30:58 +0530

    Time: 45 ms (26.99%), Space: 47.1 MB (40.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,22 @@
-import java.util.*;
+//import java.util.Arrays;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j}; // return indices
+                }
             }
-
-            map.put(nums[i], i);
         }
+        return new int[]{}; // return empty if no pair found
+    }
 
-        return new int[]{-1, -1}; // not required in LC (always one solution)
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int result[] = sol.twoSum(arr, 7);
+        
+        System.out.println(Arrays.toString(result));
     }
 }
